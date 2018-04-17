c22e008641539c85e2cad58d5c94dc6e2b44a24f
Kazunori Kimura <kimura.allwin@gmail.com>
2018-04-17 17:58:23 +0900

    mod font size

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
 namespace App1
@@ -22,7 +25,27 @@ namespace App1
             // 東京駅を表示
             MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(MapInitialPosition, MapDistance));
 
-            // TODO Geolocatorのパーミッションチェック
+            StartLatitude.Text = "35.681298";
+            StartLongitude.Text = "139.766247";
+
+            // Geolocatorのパーミッションチェック
+            ChackLocationPermissionStatusAsync(MyMap);
 		}
+
+        private async Task ChackLocationPermissionStatusAsync(Map map)
+        {
+            var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Plugin.Permissions.Abstractions.Permission.Location);
+            if (status != PermissionStatus.Granted)
+            {
+                // 許可されていなければパーミッションをリクエスト
+                var permissions = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Location);
+                status = permissions[Permission.Location];
+            }
+
+            if (status == PermissionStatus.Granted)
+            {
+                map.IsShowingUser = true;
+            }
+        }
 	}
 }
