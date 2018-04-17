302399d5a9b08b76f642bc3f921551be4a803044
Kazunori Kimura <kimura.allwin@gmail.com>
2018-04-17 15:55:12 +0900

    first commit.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+	{
+        /// <summary>
+        /// マップの初期位置（東京駅）
+        /// </summary>
+        private readonly Position MapInitialPosition = new Position(35.681298, 139.766247);
+
+        /// <summary>
+        /// マップの表示距離
+        /// </summary>
+        private readonly Distance MapDistance = Distance.FromMiles(0.3);
+
+        public MainPage()
+		{
+			InitializeComponent();
+
+            // 東京駅を表示
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(MapInitialPosition, MapDistance));
+
+            // TODO Geolocatorのパーミッションチェック
+		}
+	}
+}
